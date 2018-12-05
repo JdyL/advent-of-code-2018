ddbe51738070389d32ba736717b62be9e51be920
Jdyl <threetwoonezero@hotmail.com>
2018-12-06 01:42:03 +1100

    Refactor code - trying to make it more efficient

--- day-1/main.js
+++ day-1/main.js
@@ -2,6 +2,7 @@ let customModule = require('../readLocalInput');
 
 const INPUT_FILE = customModule.textToArray(customModule.getInputFile());
 
+
 const DUMMY_TEST = ['+1','-2','+3','+1','+1','-2'];
 const DUMMY_TEST_2 = ['+3','+3','+4','-2','-4'];
 const DUMMY_TEST_3 = ['-6','+3','+8','+5','-6'];
@@ -10,34 +11,47 @@ const DUMMY_TEST_4 = ['+7','+7','-2','-7','-4'];
 //// Part 2 variables
 var partTwoAddition = 0;
 var partTwoArray = [];
+/////////
 
 function stringToInt(input) {
     return parseInt(input) || 0;
 }
 
-var partOne = function partOne(input) {
-    //addition of the array
-    let result = 0;
-    for (var x = 0; x<input.length;x++) {
-        if (input[x] !== '') {
-        result += stringToInt(input[x]);
+function numericOnlyArray(input) {
+    let inputArray = [...input];
+    input.forEach((element,index) => {
+        if (isNaN(element) || element === null || element === undefined || element == '') {
+            inputArray.splice(index,1);
         }
-    }
+    });
+
+    inputArray = inputArray.map(x => stringToInt(x));
+
+    return inputArray;
+}
+
+var partOne = function partOne(input) {
+     //addition of the array
+    input = numericOnlyArray(input);
+    let result = 0;
+    input.map(num => result += num);
     return result;
 }
 
 var partTwo = function partTwo(input) {
+    input = numericOnlyArray(input);
+
     for (var x = 0; x<input.length;x++) {
-        if (input[x] !== '') {
-            partTwoAddition += stringToInt(input[x]);
-            partTwoArray.push(partTwoAddition);
 
-            let checkOccurence = findSameOccurence(partTwoArray, partTwoAddition);
+        partTwoAddition += stringToInt(input[x]);
+        partTwoArray.push(partTwoAddition);
+        
+        let checkOccurence = findSameOccurence(partTwoArray, partTwoAddition);
 
-            if (!(checkOccurence === false)) {
-                return checkOccurence;
-            }
+        if (!(checkOccurence === false)) {
+            return checkOccurence;
         }
+        
     }
 
     return partTwo(input);
@@ -63,6 +77,7 @@ try {
     // console.log('The answer is:', partTwo(DUMMY_TEST_4));
     // console.log('The answer is:', partTwo(INPUT_FILE));
     console.log(partTwo(INPUT_FILE));
+    // numericOnlyArray(['123','2','2',''])
 }
 catch(e) {
     console.log("Something went wrong ", e);
