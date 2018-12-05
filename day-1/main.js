2257e8232d2fe27712296d88becea8db6723cc02
Jdyl <threetwoonezero@hotmail.com>
2018-12-05 20:43:14 +1100

    Finish Day 1 Part 1

--- day-1/main.js
+++ day-1/main.js
@@ -2,9 +2,23 @@ let customModule = require('../readLocalInput');
 
 const INPUT_FILE = customModule.textToArray(customModule.getInputFile());
 
-const DUMMY_TEST = ['+14','1','-12'];
+const DUMMY_TEST = ['+14','+1','-12','-17','+17','-1'];
 
-function stringToInt(DUMMY_TEST) {
-    return parseInt(DUMMY_TEST);
+function stringToInt(input) {
+    return parseInt(input) || 0;
 }
 
+var calculate = function calculate(input) {
+    var result = 0;
+    try {
+        for (x in input) {
+            result += stringToInt(input[x]);
+        }
+        return result;
+    }
+    catch(e) {
+        console.log("Something went wrong ", e);
+    }
+}
+
+console.log(calculate(INPUT_FILE));
